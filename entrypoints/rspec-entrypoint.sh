147b5d0206bda9a6df1046c058386fa767eb0bad
papapathe <94769291+papapathe@users.noreply.github.com>
2022-02-12 16:32:38 -0600

    Fix github actions (#10)
    * Add quay.io to the registries we push our images
    
    * GH-1 bootstrap swagger docs for students
    
    * GH-1 Fix rspec entrypoint
    
    * GH-1 update puma from 5.5.2 to 5.6.2
    
    Name: puma
    Version: 5.5.2
    CVE: CVE-2022-23634
    GHSA: GHSA-rmj8-8hhh-gv5h
    Criticality: High
    URL: https://github.com/puma/puma/security/advisories/GHSA-rmj8-8hhh-gv5h
    Title: Information Exposure with Puma when used with Rails
    Solution: upgrade to ~> 4.3.11, >= 5.6.2
    
    * Gh-1 update actionpack
    
    Name: actionpack
    Version: 6.1.4.4
    CVE: CVE-2022-23633
    GHSA: GHSA-wh98-p28r-vrc9
    Criticality: Unknown
    URL: https://groups.google.com/g/ruby-security-ann/c/FkTM-_7zSNA/m/K2RiMJBlBAAJ
    Title: Possible exposure of information vulnerability in Action Pack
    Solution: upgrade to ~> 5.2.6, >= 5.2.6.2, ~> 6.0.4, >= 6.0.4.6, ~> 6.1.4, >= 6.1.4.6, >= 7.0.2.2
    
    * Gh-1 add permissions to gh action for read/write ghcr packages
    
    * GH-1 add config file for deepsource auto MR reviewer
    
    * GH-1 Split docker workflows in two different gh actions
    
    * GH-1 add a gh action for version releases
    
    Co-authored-by: Pathe SENE <pathe.sene@gmail.com>

--- entrypoints/rspec-entrypoint.sh
+++ entrypoints/rspec-entrypoint.sh
@@ -6,5 +6,5 @@ if [ -f tmp/pids/server.pid ]; then
     rm tmp/pids/server.pid
 fi
 
-bundle exec rails db:create db:migrate
+bundle exec rails db:drop db:create db:migrate
 bundle exec guard --force-polling
