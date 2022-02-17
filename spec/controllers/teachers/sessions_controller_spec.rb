c031c75a6a67a6f59475761e1796c8545628a842
Pathe SENE <pathe.sene@gmail.com>
2022-02-16 21:45:17 -0600

    GH-5 Refactored controller specs with table testings

--- spec/controllers/teachers/sessions_controller_spec.rb
+++ spec/controllers/teachers/sessions_controller_spec.rb
@@ -2,49 +2,39 @@
 
 require 'rails_helper'
 
+
+BadLoginParams = Struct.new(:identifier, :password, :expected_status_code)
+BAD_LOGIN_TABLES = [
+  BadLoginParams.new('',  '', :precondition_failed),
+  BadLoginParams.new('badidentifier.com', '', :precondition_failed),
+  BadLoginParams.new(FactoryBot.create(:db_teacher, :with_status_active).email, 'bad_password', :precondition_failed)
+]
+
 RSpec.describe Teachers::SessionsController do
   describe '#create' do
+    let(:params) { { identifier: identifier, password: password } }
     before { post :create, params: params }
 
-    let(:params) { { identifier: identifier, password: password } }
+    BAD_LOGIN_TABLES.each do |table_test|
+      context "bad empty params #{table_test.inspect}" do
+        let(:identifier) { table_test.identifier }
+        let(:password) { table_test.password }
 
-    context 'with empty params' do
-      let(:identifier) {}
-      let(:password) {}
-
-      it 'responds with unprocessable entity' do
-        expect(response).to have_http_status(:precondition_failed)
-      end
-    end
-
-    context 'with invalid email address format' do
-      let(:identifier) { 'badidentifier.com' }
-      let(:password) {}
-
-      it 'responds with precondition failed' do
-        expect(response).to have_http_status(:precondition_failed)
-      end
-    end
-
-    context 'with valid email address format and bad password' do
-      let(:identifier) { 'bad@identifier.com' }
-      let(:password) { 'badpassword' }
-
-      it 'responds with precondition failed' do
-        expect(response).to have_http_status(:precondition_failed)
+        it 'responds with unprocessable entity' do
+          expect(response).to have_http_status(table_test.expected_status_code)
+        end
       end
     end
 
     context 'with email address and valid password ' do
-      let(:teacher) { FactoryBot.create(:db_teacher) }
+      let(:teacher) { FactoryBot.create(:db_teacher, :with_status_active) }
       let(:identifier) { teacher.email }
       let(:password) { teacher.password }
+      let(:parsed_body) { JSON.parse(response.body).with_indifferent_access }
 
       it { expect(response).to have_http_status(:created) }
-
       it 'will return a json encoded response' do
-        json = JSON.parse(response.body).with_indifferent_access
-        expect(json[:token]).not_to be_nil
+        expect(parsed_body[:token]).not_to be_nil
       end
     end
   end
