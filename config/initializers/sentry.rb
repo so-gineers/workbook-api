2922a2c3df57ee933ad4c92afb1d04009b25c59d
Pathe SENE <pathe.sene@gmail.com>
2022-02-18 19:59:24 -0600

    GH-5 add sentry for error tracking

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,17 @@
+Sentry.init do |config|
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  config.background_worker_threads = 5
+  config.before_send = lambda do |event, hint|
+    ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(event.to_hash)
+  end
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = %w[production heroku]
+  config.excluded_exceptions += ['ActionController::RoutingError', 'ActiveRecord::RecordNotFound']
+end
