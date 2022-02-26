69647fece679b83aff231167e70658b873ccab72
Pathe SENE <pathe.sene@gmail.com>
2022-02-26 11:29:51 -0600

    GH-5 set heroku env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  ENV['APP_HOSTS'].split.each do |host|
+  ENV['WORKBOOK_APP_HOSTS'].split.each do |host|
     config.hosts << host
   end
 end
