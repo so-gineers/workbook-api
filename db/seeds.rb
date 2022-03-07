1c76be4d8a4d46487e79980c2be43ff0ad9584df
Pathe SENE <pathe.sene@gmail.com>
2022-03-07 07:33:51 -0600

    GH-5 add seeds for teachers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # Do we really need to do this
+#
+
+
+1000.times do
+  teachers = (1..1000).to_a.map {
+    FactoryBot.build(:db_teacher).attributes.merge("id" => SecureRandom.uuid)
+  }
+  Db::Teacher.insert_all!(teachers)
+end
+
