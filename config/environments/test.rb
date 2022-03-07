c9816455c9479029d8e8ec8f3ffd035df9d625bf
Pathe SENE <pathe.sene@gmail.com>
2022-03-07 07:31:21 -0600

    GH-1 do not cache classes in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,8 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.cache_classes = true
-
+  config.cache_classes = false
   config.eager_load = false
 
   config.public_file_server.enabled = true
